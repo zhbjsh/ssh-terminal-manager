21c6e09426068ec83006ab6249055e4169850165
zhbjsh <zhbjsh@outlook.com>
2025-02-22 21:19:23 +0100

    Remove logs from event handlers

--- src/ssh_terminal_manager/state.py
+++ src/ssh_terminal_manager/state.py
@@ -35,31 +35,24 @@ class State:
         self.on_change.notify(self)
 
     def handle_ping_error(self) -> None:
-        self._manager.logger.debug("%s: Ping error", self._manager.name)
         self.online = False
 
     def handle_ping_success(self) -> None:
-        self._manager.logger.debug("%s: Ping success", self._manager.name)
         self.online = True
 
     def handle_auth_error(self) -> None:
-        self._manager.logger.debug("%s: Authentication error", self._manager.name)
         self._manager.reset_commands()
         self.error = True
 
     def handle_connect_error(self) -> None:
-        self._manager.logger.debug("%s: Connect error", self._manager.name)
         self._manager.reset_commands()
 
     def handle_connect_success(self) -> None:
-        self._manager.logger.debug("%s: Connect success", self._manager.name)
         self.connected = True
         self.error = False
 
     def handle_disconnect(self) -> None:
-        self._manager.logger.debug("%s: Disconnected", self._manager.name)
         self.connected = False
 
     def handle_execute_error(self) -> None:
-        self._manager.logger.debug("%s: Execute error", self._manager.name)
         self._manager.reset_commands()
